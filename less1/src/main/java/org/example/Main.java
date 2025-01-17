4ef1433d44633aaabef72f215ef105b8e3f9faf6
leon <efimov.lew@gmail.com>
2025-01-17 23:36:40 +0300

    less 1

---
+++ less1/src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //принимаем количество минут на вход, через параметр
+        //System.out.println("n: " + args[0]);
+        int n = Integer.parseInt(args[0]);
+
+        //вычислим количество полных часов
+        int hours = n / 60;
+        //System.out.println("hours: " + hours);
+
+        //вычислим количество минут, после вычета полных часов
+        int mins = n - hours * 60;
+
+        //если количество минут больше суток, вычислим количество полных суток
+        int days = hours / 24;
+        //System.out.println("days: " + days);
+
+        //вычислим количество часов только в рамках последних суток
+        hours = hours - 24 * days;
+        //System.out.println("hours2: " + hours);
+
+        //выведем результат
+        String h = "";
+        String m = "";
+        if (hours < 10) {
+            h = "0" + hours;
+        } else {
+            h = "" + hours;
+        }
+        if (mins < 10) {
+            m = "0" + mins;
+        } else {
+            m = "" + mins;
+        }
+        System.out.println(h + ":" + m);
+
+    }
+}
+
