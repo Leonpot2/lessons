846773e31bab2052e4bc2ecbb9ece926df43adab
leon <efimov.lew@gmail.com>
2025-01-18 15:53:42 +0300

    less 2

--- less2/src/main/java/org/example/Main.java
+++ less2/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n = scanner.nextInt();
+
+        int m = scanner.nextInt();
+
+        int k = scanner.nextInt();
+
+        if ((n * m) / 2 == k) {
+            System.out.println("Yes");
+
+        } else {
+            System.out.println("No");
+        }
     }
 }
